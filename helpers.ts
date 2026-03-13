aa244c7f2fdb5b886bdc256708c25253031fe368
Ataurelius <98280485+Ataurelius@users.noreply.github.com>
2026-03-13 20:54:38 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T17:54:13.336Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-13T17:54:37.959Z
+Commit: Fix memory leak in event handlers

728315c5d7d503e349caf9d8f58b829178dcdb07
Ataurelius <98280485+Ataurelius@users.noreply.github.com>
2026-03-13 20:54:14 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T17:53:58.264Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-13T17:54:13.336Z
+Commit: Fix CSS styling bug on mobile devices

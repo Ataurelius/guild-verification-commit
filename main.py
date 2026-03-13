871cdda5375f07faa4222acfb33c1dd700f7e9c2
Ataurelius <98280485+Ataurelius@users.noreply.github.com>
2026-03-13 20:55:19 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T17:55:17.592Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-13T17:55:18.671Z
+Commit: Fix memory leak in event handlers

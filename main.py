81c52d618cdefa601665b9cdc11847fc547ffcb0
Ataurelius <98280485+Ataurelius@users.noreply.github.com>
2026-03-13 20:55:53 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T17:55:50.335Z
-Commit: Add dark mode support
+Timestamp: 2026-03-13T17:55:52.742Z
+Commit: Resolve null pointer exception in user service

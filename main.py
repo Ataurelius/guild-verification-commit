9937220ea1cd815a48cbeb543dc59c2485281b78
Ataurelius <98280485+Ataurelius@users.noreply.github.com>
2026-03-13 20:55:18 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T17:55:12.319Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-13T17:55:17.592Z
+Commit: Resolve null pointer exception in user service

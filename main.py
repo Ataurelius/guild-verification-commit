308dd76cc7880bd83104cadc2dd55668e4a22b47
Ataurelius <98280485+Ataurelius@users.noreply.github.com>
2026-03-13 20:55:06 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T17:54:59.127Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-13T17:55:06.015Z
+Commit: Add multi-language support
